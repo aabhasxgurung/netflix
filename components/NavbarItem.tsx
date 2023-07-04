d93cc95dbcabe5ffa45e5b699f9a0a369ac257b4
Axbhxs <aabhasgrg46@gmail.com>
2023-07-04 16:54:56 +0545

    final commit for the day

--- components/NavbarItem.tsx
+++ components/NavbarItem.tsx
@@ -1,5 +1,13 @@
-const NavbarItem = () => {
-  return <div className="text-white cursor-pointer"></div>;
+interface NavbarItemProps {
+  label: string;
+}
+
+const NavbarItem: React.FC<NavbarItemProps> = ({ label }) => {
+  return (
+    <div className="text-white cursor-pointer hover:text-gray-300 transition">
+      {label}
+    </div>
+  );
 };
 
 export default NavbarItem;
